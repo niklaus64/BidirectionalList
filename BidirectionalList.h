c5bb92be01665dbc31a2257cbf86d89319055b82
mdjdrn1 <mdjdrn1@gmail.com>
2018-04-17 10:03:58 +0200

    Changed to raw ptr

--- BidirectionalList.h
+++ BidirectionalList.h
@@ -3,23 +3,20 @@
 
 struct Node
 {
-	std::shared_ptr<Node> prev;
-	std::shared_ptr<Node> next;
+	Node* prev;
+	Node* next;
 	double value;
 };
 
 class BidirectionalList
 {
-	using NodePtr = std::shared_ptr<Node>;
 	using Range = std::pair<uint32_t, uint32_t>;
 public:
 	BidirectionalList(std::pair<uint32_t, uint32_t>);
 	~BidirectionalList();
 
-	NodePtr head, tail;
-
 	void push(double);
-
 private:
+	Node *head, *tail;
 	Range listRange;
 };
